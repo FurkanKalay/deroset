c2ac8b6359e3a384122339ac02fb2262847e9ed0
Furkan <furkaneminkalay@gmail.com>
2022-10-11 13:13:54 +0200

    basic html

---
+++ footer.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <footer>
+        <div class="adres">
+            <p>Ons adres:</p>
+            <p>9184LK</p>
+            <p>Mannengrottenstraat 77</p>
+            <p>Castricum</p>
+        </div>
+        <div class="bezorg">
+            <p>Wij bezorgen in</p>
+            <p>Haarlem</p>
+            <p>Castricum</p>
+            <p>Hoofddorp</p>
+        </div>
+        <div class="voorwaarden">
+            <p><a href="">Onze voorwaarden</a></p>
+        </div>
+    </footer>
+</body>
+
+</html>
