eaf2415af958fbc54d4a254a497388e8bf808043
Furkan <furkaneminkalay@gmail.com>
2022-10-26 09:57:15 +0200

    bezig met form

--- winkelmand.php
+++ winkelmand.php
@@ -25,12 +25,50 @@ include 'php/database.php';
 ?>
 
     <div class="front">
+        <div class="winkelmanden">
 
-        <div id="winkelmand" class="winkelmand">
-            <div class="buttondelete" id="buttondelete" onclick="deleteProduct()">
-                delete all
+            <div id="winkelmand" class="winkelmand">
+                <div class="buttondelete" id="buttondelete" onclick="deleteProduct()">
+                    delete all
+                </div>
             </div>
+            <form>
+                <label for="fname">Naam</label>
+                <input type="text" id="fname" name="fname" value="John">
+                <label for="lname">Adres</label>
+                <input type="text" id="lname" name="lname" value="Doe">
+
+                <label for="fname">Postcode</label>
+                <input type="text" id="fname" name="fname" value="John">
+                <label for="lname">Plaats</label>
+                <input type="text" id="lname" name="lname" value="Doe">
+
+                <label for="lname">Telefoonnummer </label>
+                <input type="text" id="lname" name="lname" value="Doe">
+
+
+
+
+
+                <div class="bezorgenophalen">
+                      <input type="radio" id="css" name="fav_language" value="CSS">
+                      <label for="css">Bezorgen</label>
+                </div>
+                <div class="bezorgenophalen">
+                      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+                      <label for="javascript">Ophalen</label>
+                </div>
+
+
+
+                <button class="btn btn-light" name="submit" type="submit">Submit</button>
+            </form>
+
+
+
         </div>
+
+
     </div>
 
 
